ed22e0ee6a8d8ba64602d166d2b05e3ae0c161c5
kakimoso <k.i.moso9532@gmail.com>
2018-11-23 22:46:20 +0900

    api導入

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'slim-rails'
 gem 'html2slim'
 
+gem 'foreman'
+
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate
+
+  protected
+
+  def authenticate
+    authenticate_token || render_unauthorized
+  end
+
+  def authenticate_token
+    authenticate_with_http_token do |token, options|
+      token == 'FOO'
+    end
+  end
+
+  def render_unauthorized
+    # render_errors(:unauthorized, ['invalid token'])
+    obj = { message: 'token invalid' }
+    render json: obj, status: :unauthorized
+  end
+
 end
